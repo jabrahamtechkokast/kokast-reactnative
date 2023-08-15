1bbe1ec22a07587a3d9d37322a6ba33a8a758e39
Jared Bartlett-Wright <jared.wbw@gmail.com>
2023-08-15 22:03:36 +1200

    ImmersiveMode settings button logic implemented

--- src/components/Modes/immersiveMode.tsx
+++ src/components/Modes/immersiveMode.tsx
@@ -5,37 +5,12 @@ import {OutputScreen} from './OutputScreen';
 import { GlobalOutputState, OutputData } from '../../store/Types';
 import { OutputGlobalStateContext } from '../../store/OutputContexts';
 import ActivateButton from './Buttons/ActivateButton';
+import ModeSettingsButton from './Buttons/ModeSettingsButton';
 
 const { width, height } = Dimensions.get('window');
 const screenWidth = width * 0.88;
 const screenHeight = height * 0.33;
 const outputWidth = screenWidth * 0.9; // 90% of the screen width
-
-interface ButtonProps {
-  text: string;
-  command: string;
-}
-
-function Button({ text, command }: ButtonProps){
-  const [isActive, setIsActive] = useState<boolean>(false);
-
-  const handlePress = () => {
-    setIsActive((prevIsActive) => !prevIsActive);
-    // Perform the fetch command or any other action you want
-    // when the button is pressed with the 'command' prop value.
-    // For example, you can use 'command' to specify the API endpoint to call.
-    // Fetch code or other actions here...
-  };
-
-  return (
-    <TouchableOpacity
-      style={[styles.button, isActive && styles.activeButton]}
-      onPress={handlePress}
-    >
-      <Text style={styles.buttonText}>{text}</Text>
-    </TouchableOpacity>
-  );
-};
 
 function ImmersiveMode() {
 
@@ -60,9 +35,9 @@ function ImmersiveMode() {
       <Text style={styles.title}>Immersive Mode</Text>
       <View style={styles.buttonsContainer}>
         {/* Use the reusable Button component */}
-        <Button text="PC Resolution" command="pc_resolution" />
+        <ModeSettingsButton setting='PC Resolution' command='TBD' modeName={modeName}/>
         <ActivateButton text="Activate" command="activate" onPress={setActiveMode} isActive={isActiveMode}/>
-        <Button text="Standard" command="standard" />
+        <ModeSettingsButton setting='Standard' command='TBD' modeName={modeName}/>
       </View>
     </View>
   );
